98f295ce5dee76248ca5d227f57614a08dc4854a
REXI38 <98491279+REXI38@users.noreply.github.com>
2024-11-08 16:19:11 +0100

    Update EMSY_TP1.c

--- EMSY_TP1.c
+++ EMSY_TP1.c
@@ -1,26 +1,34 @@
 //-----------------------------------------------------------------------------------//
 // Nom du projet 		: EMSY TP1 
 // Nom du fichier 		: EMSY_TP1.c 
-// Date de création 	: 27.09.2024
+// Date de création 	: 08.11.2024
 // Date de modification : xx.xx.20xx
 //
-// Auteur 				: Philou (Ph. Bovey)
+// Auteur 				: Louis (LCX)
 //
 // Version				: 0.1
 //
-// Description          : 
+// Description         calcule de la surface d'un carré
 //
 //
 // Remarques :           
 //----------------------------------------------------------------------------------//
 
 //--- librairie standart ---//
-#include <stdio.h>	// pour usage printf et scanf_s
-                    // Nouveauté du VC++ 2005, 2008, 2010 et 2015 : le scanf_s remplace scanf
-
-//-- programme principale --// 
-int main(void)
-{
-
-  return(0);
-}
+#include <stdio.h> 
+ int main() 
+{ 
+    float cote, surface; 
+  
+    // Demander à l'utilisateur de saisir la longueur du côté 
+    printf("Entrez la longueur du côté du carré: "); 
+    scanf("%f", &cote); 
+  
+    // Calculer la surface du carré
+    surface = cote * cote; 
+  
+    // Afficher la surface 
+    printf("La surface du carré est: %.2f\n", surface); 
+  
+    return 0; 
+} 
